d7389995c0f1056b109fd8e92f478603752ccf82
Jon Haygood <jonathan.haygood@timeinc.com>
2017-09-21 13:45:12 -0400

    Create plugin

---
+++ index.js
@@ -0,0 +1,22 @@
+// Don't allow typeof comparisons with "undefined" or "number"
+const VALID_TYPES = ['symbol', 'object', 'boolean', 'string', 'function'];
+const OPERATORS = ['==', '===', '!=', '!=='];
+
+module.exports = {
+  create: context => ({
+    UnaryExpression(node) {
+      if (node.type === 'UnaryExpression' && node.operator === 'typeof') {
+        const parent = context.getAncestors().pop();
+        if (parent.type === 'BinaryExpression' && OPERATORS.includes(parent.operator)) {
+          const sibling = parent.left === node ? parent.right : parent.left;
+          if (sibling.type === 'Literal' || sibling.type === 'TemplateLiteral' && !sibling.expressions.length) {
+            const value = sibling.type === 'Literal' ? sibling.value : sibling.quasis[0].value.cooked;
+            if (!VALID_TYPES.includes(value)) {
+              context.report({node: sibling, message: `${value} is not permitted in typeof operation`});
+            }
+          }
+        }
+      }
+    }
+  })
+};
