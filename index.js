ef548c3f1acfd3d851596862c629e02967c933dc
Jon <haygoodjon@gmail.com>
2017-10-10 18:37:53 -0400

    Test

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-// Don't allow typeof comparisons with "undefined" or "number"
-const VALID_TYPES = ['symbol', 'object', 'boolean', 'string', 'function', 'number'];
-const OPERATORS = ['==', '===', '!=', '!=='];
 
 module.exports.rules = {
-  undefined: context => ({
-    UnaryExpression(node) {
-      if (node.type === 'UnaryExpression' && node.operator === 'typeof') {
-        const parent = context.getAncestors().pop();
-        if (parent.type === 'BinaryExpression' && OPERATORS.includes(parent.operator)) {
-          const sibling = parent.left === node ? parent.right : parent.left;
-          if (sibling.type === 'Literal' || sibling.type === 'TemplateLiteral' && !sibling.expressions.length) {
-            const value = sibling.type === 'Literal' ? sibling.value : sibling.quasis[0].value.cooked;
-            if (!VALID_TYPES.includes(value)) {
-              context.report({node: sibling, message: `${value} is not permitted in typeof operation`});
-            }
-          }
+  functions: context => ({
+    FunctionExpression(node) {
+      if (node.type === 'FunctionExpression') {
+        // is single line function
+        if (node.body && node.body.body && node.body.body.length === 1 && node.body.body[0].expression.type === 'CallExpression') {
+          const params = node.body.body[0].expression.arguments;
+          console.info(params)
+          context.report(node,  'function looks like a verbose wrapper');
         }
       }
     }
